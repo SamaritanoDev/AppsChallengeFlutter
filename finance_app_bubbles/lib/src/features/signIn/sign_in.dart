9035bd76ad938d009ddcc0792e217eb727e8d6e8
Lesly Samaritano Ayala <1628112@utp.edu.pe>
2024-04-15 00:32:12 -0500

    feat: form sign in start

--- finance_app_bubbles/lib/src/features/signIn/sign_in.dart
+++ finance_app_bubbles/lib/src/features/signIn/sign_in.dart
@@ -1,3 +1,5 @@
+import 'package:finance_app_bubbles/src/utils/backgroung_cloud.dart';
+import 'package:finance_app_bubbles/src/utils/text_field_custom.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatelessWidget {
@@ -5,13 +7,38 @@ class SignIn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final color = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+
+    //styles
+    final titleStyle = textTheme.headlineLarge?.copyWith(
+      color: color.onPrimaryContainer,
+      fontFamily: 'latoRegular',
+    );
+    final subtitleStyle = textTheme.titleSmall?.copyWith(
+      color: const Color(0xff666464),
+      fontFamily: 'latoSemiBold',
+    );
+
     return Scaffold(
-      appBar: AppBar(
-        forceMaterialTransparency: true,
-        title: const Text('Sign In'),
-      ),
-      body: const Center(
-        child: Text('Sign In'),
+      body: BackgroundCloud(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(height: 55),
+            Text('Sign in', style: titleStyle),
+            const SizedBox(height: 17),
+            Text(
+              'Hi ! Welcome back, you have been missed.',
+              style: subtitleStyle,
+            ),
+            const SizedBox(height: 58),
+            const TextFieldCustom(labelTextField: 'Email'),
+            const SizedBox(height: 21),
+            const TextFieldCustom(labelTextField: 'Password'),
+          ],
+        ),
       ),
     );
   }
---
+++ finance_app_bubbles/lib/src/utils/text_field_custom.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class TextFieldCustom extends StatelessWidget {
+  final String labelTextField;
+  const TextFieldCustom({
+    super.key,
+    required this.labelTextField,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+
+    //styles
+    final labelStyle = textTheme.titleMedium?.copyWith(
+      color: color.primary,
+      fontFamily: 'latoRegular',
+    );
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(labelTextField, style: labelStyle),
+        const SizedBox(height: 5),
+        const SizedBox(
+          width: 339,
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(8.0)),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
